2311b6c682956b7d39f7497c0b2f0c074bbab963
Aldaris <z919590416@live.cn>
2018-03-02 11:31:48 +0800

    正确的设置"/"

--- src/main/java/com/crocoro/SunHttpServer.java
+++ src/main/java/com/crocoro/SunHttpServer.java
@@ -38,7 +38,7 @@ public class SunHttpServer {
     public void start() throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
         addContext(server, "res");
-        server.createContext("/" + defaultInterface, new FileHandler("res/" + defaultInterfaceFile));
+        server.createContext("/", new FileHandler("res/" + defaultInterfaceFile));
         server.createContext("/api", new StatusHandler(passwd));
         server.start();
         System.out.println("启动完成 可以访问 http://IP:" + port + "/status 查看");
